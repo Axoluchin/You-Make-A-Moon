56355571afcb9eae08118edc85729ae2403e7899
Damian Huchin <huchin.h.damian@gmail.com>
2022-11-04 22:17:43 -0600

    Little changes in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Navbar, Text, Switch, useTheme } from "@nextui-org/react";
+import { Navbar, Text, Switch, useTheme, Button } from "@nextui-org/react";
 import { useTheme as useNextTheme } from "next-themes";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
 
@@ -7,29 +7,41 @@ const Header = () => {
   const { setTheme } = useNextTheme();
   const { isDark } = useTheme();
   return (
-    <Navbar
-    isBordered
-    >
+    <Navbar isBordered>
       <Navbar.Brand>
         <Text h3 style={{ marginBottom: 0 }}>
           You Make A Moon
         </Text>
       </Navbar.Brand>
-      <Navbar.Content>
-        <Link href="/">
-          <Text>Make a Moon</Text>
-        </Link>
-        <Navbar.Link isDisabled>Dashboard</Navbar.Link>
+      <Navbar.Content enableCursorHighlight>
+        <Navbar.Item>
+          <Link href="/">
+            <Text>Make a Moon</Text>
+          </Link>
+        </Navbar.Item>
+        <Navbar.Item>
+          <Link href="/">
+            <Text>Dashboard</Text>
+          </Link>
+        </Navbar.Item>
       </Navbar.Content>
       <Navbar.Content>
         <Switch
           checked={isDark}
           onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-          iconOn={<MdDarkMode/>}
-          iconOff={<MdLightMode/>}
+          iconOn={<MdDarkMode />}
+          iconOff={<MdLightMode />}
         />
-        <Navbar.Link isDisabled>Inicar Sesión</Navbar.Link>
-        <Navbar.Link isDisabled>Registrarse</Navbar.Link>
+        <Navbar.Item>
+          <Button auto light size="sm">
+            Iniciar Sesión
+          </Button>
+        </Navbar.Item>
+        <Navbar.Item>
+          <Button auto flat>
+            Registrarse
+          </Button>
+        </Navbar.Item>
       </Navbar.Content>
     </Navbar>
   );
